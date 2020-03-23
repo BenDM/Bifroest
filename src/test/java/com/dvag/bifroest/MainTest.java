ea56998f7359d60b4bb52e21c02c11289585b292
BenDM <simo2332001@yahoo.fr>
2020-03-23 12:05:33 +0100

    update1334901334kwhhtl

--- src/test/java/com/dvag/bifroest/MainTest.java
+++ src/test/java/com/dvag/bifroest/MainTest.java
@@ -2,6 +2,7 @@ package com.dvag.bifroest;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import cucumber.api.junit.Cucumber;
 import net.masterthought.cucumber.ReportBuilder;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.Reportable;
@@ -9,6 +10,7 @@ import net.masterthought.cucumber.json.support.Status;
 import net.masterthought.cucumber.sorting.SortingMethod;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -16,6 +18,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+@RunWith(Cucumber.class)
 public class MainTest {
     @Test
     void testParallel() {
